018d80baff1de024dd7c5e24f5f07ec0000d37c6
Alena Nemtsova <jelenatbs@gmail.com>
2024-02-07 23:42:16 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+class First {
+    constructor() {
+
+    }
+
+    hello() {
+        console.log("Привет я метод родителя!")
+    }
+}
+
+class Second extends First {
+    constructor() {
+        super()
+    }
+
+    hello() {
+        super.hello()
+        console.log("А я наследуемый метод!")
+    }
+}
+
+const newClass = new Second();
+newClass.hello();
